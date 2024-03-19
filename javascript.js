39c3da6bded3b619e349cd3fbbabc3bf8152a334
Hansespinosa2 <andresespinosa@ufl.edu>
2024-03-19 02:36:47 -0400

    Finish up until extra credit

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
 const divContainer = document.querySelector('div.container');
+const btnReset = document.querySelector('button.btn-reset');
+
+btnReset.addEventListener('click', function() {
+    const divAll = document.querySelectorAll('.container>div')
+    divAll.forEach(function(div) {
+        div.style['background-color'] = 'white';
+    })
+});
 
 for (let i = 0; i <16*16; i ++) {
     const aDiv = document.createElement('div')
     aDiv.setAttribute('class',`div${i}`)
     aDiv.style['border'] = '1px solid black';
-    aDiv.textContent = `This is div #${i}`
+    aDiv.textContent = ``
+    aDiv.addEventListener('mouseenter', function (){
+        this.style['background-color'] = 'black';
+    })
+
     divContainer.append(aDiv)
 }
